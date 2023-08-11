e6e99db7963be3588dc8da3ad649956b433cc7ce
Ankit kumar singh <132122896+Ankit4257@users.noreply.github.com>
2023-08-11 12:05:39 +0530

    Add files via upload

---
+++ app/src/main/java/com/gigawattstechnology/induction/GameActivity.kt
@@ -0,0 +1,13 @@
+package com.gigawattstechnology.induction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+
+    }
+}
---
+++ app/src/main/java/com/gigawattstechnology/induction/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.gigawattstechnology.induction
+
+import android.content.DialogInterface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.RadioButton
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
+
+
